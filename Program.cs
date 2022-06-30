11b27454a900e24b4e73d61d5c15e06e1d24cc88
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-30 19:52:02 -0300

    add com cor no refrao e versos organizados

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Writeline ("[Intro: yung lixo]");
+Console.Writeline ("Derãoderão, não");
+Console.Writeline ("Derão, não");
+Console.Writeline ("Derãoderão, não");
+Console.Writeline ("Derão, não");
+Console.Writeline ("Derãoderão, não");
+Console.Writeline ("Olha só, ó, ó, ó");
+
+Console.Writeline ("[Verso 1: yung lixo]");
+Console.Writeline ("Invadindo a França, ah, assalto de grande porte");
+Console.Writeline ("Tipo entrar na Casa Branca e levar tudo que nói' pode");
+Console.Writeline ("No topo do Louvre descendo de corda memo'");
+Console.Writeline ("Hoje nói' vai levar tudo e tamo' pouco se fodendo (Ha!)");
+Console.Writeline ("Minha vó quer jantar debaixo da última ceia");
+Console.Writeline ("Então vou levar pra ela de presente na sexta");
+Console.Writeline ("E se ela fizer uma desfeita, mano, não vai ter problema");
+Console.Writeline ("Levo mais uma dezena que é só pra ter certeza");
+
+Console.Writeline ("[Refrão: yung lixo]" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Da Vinci Code, bitch, mano, nóis tá invisível" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Incrível na noite o kit, mano, é Missão Impossível" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Então aumenta a segurança, então, corre e fecha as tranca" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Que se os moleque chegar, eles rapa c'a tua banca" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Da Vinci Code, bitch, mano, nóis tá invisível" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Incrível na noite o kit, mano, é Missão Impossível" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Então aumenta a segurança, então, corre e fecha as tranca" foreground color = ConsoleColor.Orange); 
+Console.Writeline ("Que se os moleque chegar, eles rapa c'a tua banca (E toma tudo!)" foreground color = ConsoleColor.Orange); 
+
+Console.Writeline ("[Verso 2: Biffe]");
+Console.Writeline ("A segurança tá fraca, nós entrou e levou tudo");
+Console.Writeline ("As trava que protegia, nós desfez com um canudo");
+Console.Writeline ("Desnudo, o Homem Vitruviano, nós carregamo'");
+Console.Writeline ("E levamo' pra nossa vó que agora tá decifrando");
+Console.Writeline ("Da Vinci Code, bitch, tu tá fudido Da Vinci");
+Console.Writeline ("Pegamo' teu autorretrato, vamo' desvendar o barato");
+Console.Writeline ("A equipe cheia dos kit, pronta pra desvendar o fato");
+Console.Writeline ("E ter fama pelas street, agora nós fica rich, bitch'");
+
+Console.Writeline ("[Refrão: yung lixo]");
+Console.Writeline ("Da Vinci Code, bitch, mano, nóis tá invisível" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Incrível na noite o kit, mano, é Missão Impossível" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Então aumenta a segurança, então, corre e fecha as tranca" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Que se os moleque chegar, eles rapa c'a tua banca" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Da Vinci Code, bitch, mano, nóis tá invisível" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Incrível na noite o kit, mano, é Missão Impossível" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Então aumenta a segurança, então, corre e fecha as tranca" foreground color = ConsoleColor.Orange);
+Console.Writeline ("Que se os moleque chegar, eles rapa c'a tua banca (E toma tudo!)" foreground color = ConsoleColor.Orange);
